bb7c4349b4af9123020ccea1f613835e4569bad4
icharlem <charles.meruwoma@gmail.com>
2025-09-05 22:17:04 -0500

    Initial commit

---
+++ test/amzn-s3-uploader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmznS3Uploader from '../lib/amzn-s3-uploader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amzn-s3-uploader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmznS3Uploader.AmznS3UploaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
